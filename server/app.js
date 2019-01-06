eea458c4c0f44d2cda79c4b8a6dd7784018853de
justisGipson <justis710@gmail.com>
2019-01-06 16:53:54 -0500

    server stuff

--- server/app.js
+++ server/app.js
@@ -5,4 +5,8 @@ var sequelize = require('./db');
 
 sequelize.sync();
 
-app.use('/test', test)
+app.listen(3000, function(){
+    console.log('App is listening on 3000')
+});
+
+app.use('/test', test);
--- server/controllers/testcontroller.js
+++ server/controllers/testcontroller.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var sequelize = require('../db');
+var TestModel = sequelize.import('../models/test');
+
+// router.get('/', function (req, res) {
+//     res.send('Hey!!! This is a test route!')
+//    });
 
 router.post('/one', (req, res) => {
-    res.send("Test 1 went through")
+    res.send("Got post message")
+});
+
+router.post('/two', (req, res) => {
+    let testData = 'Test data for endpoint two';
+
+    TestModel.create({
+        testdata: testData
+    }).then(dataFromDatabase => {
+        res.send('Test two went through')
+    })
 });
 
 module.exports = router;
