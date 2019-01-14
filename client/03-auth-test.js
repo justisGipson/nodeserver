1110595ee603bef352839f37bca2b3c60270c8c8
justisGipson <justis710@gmail.com>
2019-01-14 16:31:13 -0500

    nodeserver DONE

--- client/03-auth-test.js
+++ client/03-auth-test.js
@@ -60,4 +60,101 @@ updateItem = () => {
     let postIdNumber = document.getElementById('updateNumber').value;
     let authTestDataInput = document.getElementById('updateValue').value;
     const fetch_url = `http://localhost:3000/authtest/update/${postIdNumber}`
+    const accessToken = localStorage.getItem('SessionToken')
+
+    let authInputData = {authtestdata: {item: authTestDataInput}};
+    const response = fetch(fetch_url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        },
+        body: JSON.stringify(authInputData)
+    }).then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data)
+        var myItem = document.getElementById('newItemValue')
+        myItem.innerHTML = data.authtestdata;
+        fetchAllFromAuthRoute();
+    })
+}
+
+deleteItem = () => {
+    let postIdNumber = document.getElementById('deleteNumber').value;
+    const fetch_url = `http://localhost:3000/authtest/delete/${postIdNumber}`;
+    const accessToken = localStorage.getItem('SessionToken');
+
+    const response = fetch(fetch_url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        }
+    }).then(response => {
+        console.log(response);
+        fetchAllFromAuthRoute();
+    })
+}
+
+deleteItemById = (paramNum) => {
+    const fetch_url = `http://localhost:3000/authtest/delete/${paramNum}`
+    const accessToken = localStorage.getItem('SessionToken')
+    const response = fetch(fetch_url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        }
+    }).then(response => {
+        console.log(response);
+        fetchAllFromAuthRoute();
+    })
+}
+
+fetchFromOneDisplayData = () => {
+    const url = 'http://localhost:3000/authtest/getall';
+    const accessToken = localStorage.getItem('SessionToken');
+
+    fetch(url, {
+        method: 'GET',
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        })
+    }).then(
+        response => {
+            return response.json()
+        }).catch(
+            error => {
+                console.log('Error:', error)
+            })
+            .then(
+                response => {
+                    let text = '';
+                    var myList = document.querySelector('ul#fourteen');
+                    while(myList.firstChild){
+                        myList.removeChild(myList.firstChild)
+                    }
+                    console.log(response);
+                    for(r of response){
+                        var listItem = document.createElement('li');
+                        var textData = r.id + ' ' + r.authtestdata;
+                        listItem.innerHTML = textData;
+                        listItem.setAttribute('id', r.id);
+                        myList.appendChild(listItem);
+                        myList.addEventListener('click', removeItem);
+                    }
+                })
+}
+
+removeItem = (e) => {
+    console.log(e);
+    var target = e.target;
+    if(target.tagName !== 'LI') return;
+    else target.parentNode.removeChild(target);
+
+    let x = target.getAttribute('id')
+    deleteItemById(x);
+    // console.log('The id number for this item is' + x);
 }
